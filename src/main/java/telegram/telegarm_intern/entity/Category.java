54a55dbf0841fc136ae14ffbb8e267bb0d461876
saidovdiyorbek <saidovdiyer777@gmail.com>
2025-01-13 16:11:46 +0500

    Added: Category

---
+++ src/main/java/telegram/telegarm_intern/entity/Category.java
@@ -0,0 +1,21 @@
+package telegram.telegarm_intern.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "category")
+@Data
+public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String name;
+
+    @Column(nullable = false)
+    private LocalDateTime createdAt;
+}
