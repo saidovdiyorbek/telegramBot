018318b855c0b3d9c35b1f60bad7cf82cb005aa6
saidovdiyorbek <saidovdiyer777@gmail.com>
2025-01-13 16:25:26 +0500

    Added: botConfig field and constructor and Bean setWebhook

--- src/main/java/telegram/telegarm_intern/config/AppConfig.java
+++ src/main/java/telegram/telegarm_intern/config/AppConfig.java
@@ -1,4 +1,21 @@
 package telegram.telegarm_intern.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.telegram.telegrambots.meta.api.methods.updates.SetWebhook;
+
+@Configuration
 public class AppConfig {
+    private final TelegramBotConfig botConfig;
+
+    public AppConfig(TelegramBotConfig botConfig) {
+        this.botConfig = botConfig;
+    }
+
+    @Bean
+    public SetWebhook setWebhookInstance(){
+        return SetWebhook.builder().url(botConfig.getWebhookPath()).build();
+    }
+
+
 }
