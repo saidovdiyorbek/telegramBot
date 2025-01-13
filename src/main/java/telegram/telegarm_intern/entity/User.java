78d761a7b467424374d423b02605a5cafc756a6f
saidovdiyorbek <saidovdiyer777@gmail.com>
2025-01-13 16:11:53 +0500

    Added: User

---
+++ src/main/java/telegram/telegarm_intern/entity/User.java
@@ -0,0 +1,40 @@
+package telegram.telegarm_intern.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import telegram.telegarm_intern.enums.Role;
+
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "users")
+@Data
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String surname;
+
+    @Column(nullable = false, unique = true)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    private Role role;
+
+    @Column(nullable = false)
+    private boolean active;
+
+    @Column(nullable = false)
+    private LocalDateTime createdAt;
+
+}
