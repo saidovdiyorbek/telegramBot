66c295cdf27fddeb5a83c80416f53718c93784d6
saidovdiyorbek <saidovdiyer777@gmail.com>
2025-01-13 16:53:19 +0500

    Added: method springWebhookBot

--- src/main/java/telegram/telegarm_intern/config/AppConfig.java
+++ src/main/java/telegram/telegarm_intern/config/AppConfig.java
@@ -3,6 +3,7 @@ package telegram.telegarm_intern.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.telegram.telegrambots.meta.api.methods.updates.SetWebhook;
+import telegram.telegarm_intern.service.TelegramFacade;
 
 @Configuration
 public class AppConfig {
@@ -15,6 +16,15 @@ public class AppConfig {
     @Bean
     public SetWebhook setWebhookInstance(){
         return SetWebhook.builder().url(botConfig.getWebhookPath()).build();
+    }
+
+    @Bean
+    public MyTelegramBot springWebhookBot(SetWebhook setWebhook, TelegramFacade telegramFacade){
+        MyTelegramBot bot = new MyTelegramBot(telegramFacade, setWebhook);
+        bot.setBotToken(botConfig.getBotToken());
+        bot.setBotUsername(botConfig.getUserName());
+        bot.setBotPath(botConfig.getWebhookPath());
+        return bot;
     }
 
 
