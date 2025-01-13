f2dfad43d74e7a67f36a6bcf362a89cd610048e1
saidovdiyorbek <saidovdiyer777@gmail.com>
2025-01-13 16:19:09 +0500

    Added: TelegramBotConfig

---
+++ src/main/java/telegram/telegarm_intern/config/TelegramBotConfig.java
@@ -0,0 +1,10 @@
+package telegram.telegarm_intern.config;
+
+import lombok.AccessLevel;
+import lombok.experimental.FieldDefaults;
+import org.springframework.stereotype.Component;
+
+@Component
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class TelegramBotConfig {
+}
