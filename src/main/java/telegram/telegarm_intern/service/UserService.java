dfaf61a41b3f2668c1789425de265d35d2238fcf
saidovdiyorbek <saidovdiyer777@gmail.com>
2025-01-14 17:05:32 +0500

    Added: UserService and create method

--- src/main/java/telegram/telegarm_intern/service/UserService.java
+++ src/main/java/telegram/telegarm_intern/service/UserService.java
@@ -1,4 +1,20 @@
 package telegram.telegarm_intern.service;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import telegram.telegarm_intern.config.MyTelegramBot;
+import telegram.telegarm_intern.dto.UserCreateDTO;
+import telegram.telegarm_intern.repository.UserRepository;
+
+@Service
+@RequiredArgsConstructor
 public class UserService {
+
+    private final UserRepository repository;
+
+
+
+    public String create(UserCreateDTO user) {
+        telegramBot.onWebhookUpdateReceived()
+    }
 }
