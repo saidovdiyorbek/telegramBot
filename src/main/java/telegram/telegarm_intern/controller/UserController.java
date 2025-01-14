6fb0a2e7d2a5ad4bcd1d2fa2a09b65b891ac736b
saidovdiyorbek <saidovdiyer777@gmail.com>
2025-01-14 17:05:06 +0500

    Added: UserController and create API

--- src/main/java/telegram/telegarm_intern/controller/UserController.java
+++ src/main/java/telegram/telegarm_intern/controller/UserController.java
@@ -1,13 +1,25 @@
 package telegram.telegarm_intern.controller;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import telegram.telegarm_intern.config.MyTelegramBot;
+import telegram.telegarm_intern.dto.UserCreateDTO;
+import telegram.telegarm_intern.service.UserService;
 
 @RestController
 @RequestMapping("/api/user")
+@RequiredArgsConstructor
 public class UserController {
-    private MyTelegramBot telegramBot;
 
+    private final UserService service;
 
+    @PostMapping("")
+    public ResponseEntity<?> create(@RequestBody UserCreateDTO user) {
+        return ResponseEntity.ok(service.create(user));
+    }
 }
