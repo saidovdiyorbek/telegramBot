432ca94919c9814ce18b1e121dbc77dbacb5bb82
saidovdiyorbek <saidovdiyer777@gmail.com>
2025-01-14 18:29:23 +0500

    Added: write body create method body

--- src/main/java/telegram/telegarm_intern/service/UserService.java
+++ src/main/java/telegram/telegarm_intern/service/UserService.java
@@ -1,20 +1,44 @@
 package telegram.telegarm_intern.service;
 
+import jakarta.ws.rs.BadRequestException;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
-import telegram.telegarm_intern.config.MyTelegramBot;
-import telegram.telegarm_intern.dto.UserCreateDTO;
+import telegram.telegarm_intern.dto.user.UserCreateDTO;
+import telegram.telegarm_intern.entity.User;
+import telegram.telegarm_intern.exceptions.UsernameAlreadyExistsException;
 import telegram.telegarm_intern.repository.UserRepository;
+
+import java.time.LocalDateTime;
+
+import static telegram.telegarm_intern.util.UserUtil.isValidPassword;
 
 @Service
 @RequiredArgsConstructor
 public class UserService {
 
     private final UserRepository repository;
+    private final PasswordEncoder passwordEncoder;
 
 
 
-    public String create(UserCreateDTO user) {
-        telegramBot.onWebhookUpdateReceived()
+    public UserCreateDTO create(UserCreateDTO userDTO) {
+        if (repository.existsByUsername(userDTO.username())) {
+            throw new UsernameAlreadyExistsException("This username is already taken");
+        }
+        if (!isValidPassword(userDTO.password())) {
+            throw new BadRequestException("Password is not valid");
+        }
+
+        User createUser = new User();
+        createUser.setName(userDTO.name());
+        createUser.setSurname(userDTO.surname());
+        createUser.setUsername(userDTO.username());
+        createUser.setPassword(passwordEncoder.encode(userDTO.password()));
+        createUser.setRole(userDTO.role());
+        createUser.setActive(true);
+        createUser.setCreatedAt(LocalDateTime.now());
+        repository.save(createUser);
+        return userDTO;
     }
 }
