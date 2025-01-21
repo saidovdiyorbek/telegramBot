ff2ef2af39ea9e8c3f97fd96812094b306fea46b
saidovdiyorbek <saidovdiyer777@gmail.com>
2025-01-22 01:58:21 +0500

    Added: Method securityFilterChain

--- src/main/java/telegram/telegarm_intern/config/SpringSecurityConfig.java
+++ src/main/java/telegram/telegarm_intern/config/SpringSecurityConfig.java
@@ -3,11 +3,28 @@ package telegram.telegarm_intern.config;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @RequiredArgsConstructor
 public class SpringSecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(hrr -> {
+                    hrr
+                            .requestMatchers("/**", "/").permitAll()
+                            .requestMatchers("/api/user/").permitAll()
+                            .anyRequest().authenticated();
+                });
+
+               http.csrf().disable();
+        return http.build();
+
+    }
 
     @Bean
     public BCryptPasswordEncoder passwordEncoder() {
